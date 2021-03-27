7f36cf46509d3b98ddeec6200e0e56e01732d493
nahaza <57981655+nahaza@users.noreply.github.com>
2021-03-27 11:32:28 +0300

    Delete ua/univer/lesson01 directory

--- ua/univer/lesson01/task04.py
+++
@@ -1,9 +0,0 @@
-hour = int(input("Enter time hour: "))
-age = int(input("Enter age: "))
-if hour < 22:
-    if age > 18:
-        print("You can buy alcohol")
-    else:
-        print("You can buy pepsi")
-else:
-    print("market is closed")
